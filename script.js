a65b15bcbd81ecdd29645a499591ab9c295dab74
Bernardo Sanchez Zapata <brnrdsz@gmail.com>
2026-06-26 13:39:29 +0200

    Ajout formulaire et affichage du résultat

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const formulaire = document.getElementById("invocation");
-formulaire.addEventListener("submit", function(event)   {
-event.preventDefault();
-console.log("clic !");
+
+formulaire.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const prenom = document.getElementById("prenom").value;
+  const creature = document.getElementById("creature").value;
+  const message = `${prenom}, ${creature} a entendu ton appel et te suivra tout ta vie !`;
+
+  document.getElementById("message").textContent = message;
+  document.getElementById("invocation").classList.add("cache");
+  document.getElementById("resultat").classList.remove("cache");
 });
